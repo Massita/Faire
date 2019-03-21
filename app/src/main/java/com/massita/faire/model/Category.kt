144316c7a1abd1e57523ecf1798de5f02fa5f9f9
Tiago Massita <s2it_tfonseca@uolinc.com>
2019-03-21 12:54:36 -0300

    Mapping the category data class

---
+++ app/src/main/java/com/massita/faire/model/Category.kt
@@ -0,0 +1,14 @@
+package com.massita.faire.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Category data class
+ * Mapping only the necessary data for this assignment
+ */
+data class Category(
+    @SerializedName("name") var name: String,
+    @SerializedName("sub_categories") var subCategories : List<Category>,
+    @SerializedName("is_product_based") var isProductBased : Boolean,
+    @SerializedName("is_featured") var isFeatured : Boolean
+)
