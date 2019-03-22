81d485332f6e31188c7138673c62308fd130bdb6
Tiago Massita <s2it_tfonseca@uolinc.com>
2019-03-22 18:49:41 -0300

    Showing brand information

--- app/src/main/java/com/massita/faire/ui/BrandFragment.kt
+++ app/src/main/java/com/massita/faire/ui/BrandFragment.kt
@@ -6,9 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.massita.faire.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_brand.*
 
 class BrandFragment : Fragment() {
+    private val args: BrandFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +20,20 @@ class BrandFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_brand, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val brand = args.brand
+
+        brandTitle.text = brand?.name
+        brandDescription.text = brand?.description
+
+        brand?.squaredImage?.let {
+            Picasso.get().load(it.url)
+                .into(brandImage)
+        }
     }
 
 
